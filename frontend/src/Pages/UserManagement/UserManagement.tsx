bcd220a21e92ee00734f9749fd65998a0c00a46a
Antonio Lindo <antoniolvitall@gmail.com>
2025-01-07 14:23:34 -0300

    feat: :construction: criando botao de mostrar barra de pesquisa

--- frontend/src/Pages/UserManagement/UserManagement.tsx
+++ frontend/src/Pages/UserManagement/UserManagement.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import UserSearchBar from '../../components'
 
 export default function UserManagement() {
+  const [search, setSearch] = useState(false);
+
+  const handleSearchButton = () => {
+    setSearch(!search)
+  };
+
   return (
     <div>
-      
+      <button onClick={handleSearchButton}>
+        Usurários
+      </button>
+      { search && <UserSearchBar /> }
     </div>
   )
 }
