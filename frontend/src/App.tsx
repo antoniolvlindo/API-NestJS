860972163e841f9b16fbe0a4e806cf508e03417d
Antonio Lindo <antoniolvitall@gmail.com>
2025-01-07 12:08:06 -0300

    feat: :rocket: estruturando projeto

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
+import UserManagement from './Pages/UserManagement/UserManagement'
 
 function App() {
-
   return (
-    <>
-
-    </>
+    <Routes>
+      <Route path="/ " element={ <UserManagement /> }/>
+    </Routes>
   )
 }
 
