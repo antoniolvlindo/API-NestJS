da5ad275142630578fe39e56a6b67d94133e0346
antoniolvlindo <antoniolvitall@gmail.com>
2025-01-13 14:17:29 -0300

    alterando ami

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI
+  ami           = "ami-00242775fc4e7fc32" # Amazon Linux 2 AMI
   instance_type = "t2.micro"
 
   tags = {
