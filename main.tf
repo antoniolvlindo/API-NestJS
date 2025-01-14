1d8278f05c16b3a0bafb21ea578f9e754ff934ea
antoniolvlindo <antoniolvitall@gmail.com>
2025-01-14 14:19:22 -0300

    testando vercel

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "vercel" {
+  api_token = var.vercel_api_token
+}
+
+resource "vercel_project" "project" {
+  name      = var.vercel_project_name
+  framework = "docker"
+}
+
+resource "vercel_deployment" "deployment" {
+  project_id = vercel_project.project.id
+  git_source {
+    type    = "github"
+    repo_id = var.repo_id
+    branch  = var.branch
+  }
+}
+
+variable "vercel_api_token" {}
+variable "vercel_project_name" {}
+variable "repo_id" {}
+variable "branch" {}
