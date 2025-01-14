9c022b97afb46589256ec4fa5b81c5b442e1a6ba
antoniolvlindo <antoniolvitall@gmail.com>
2025-01-14 14:45:47 -0300

    tudo funcionando

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,11 +9,11 @@ import { AppService } from './app.service';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '835247',
-      database: 'nestjs',
+      host: process.env.DATABASE_HOST || 'localhost',
+      port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+      username: process.env.DATABASE_USER || 'postgres',
+      password: process.env.DATABASE_PASSWORD || '835247',
+      database: process.env.DATABASE_NAME || 'nestjs',
       entities: [User],
       synchronize: true,
       logging: true,
